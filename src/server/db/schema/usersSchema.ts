21cd086eb135a283ae435fb397a998fe23e97d23
GeloxGero <gelo12gero@gmail.com>
2026-05-08 11:17:01 +0800

    added usersSchema

--- src/server/db/schema/usersSchema.ts
+++ src/server/db/schema/usersSchema.ts
@@ -1,7 +1,24 @@
 import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
 
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey(),
   name: text("name"),
-  countryId: integer("country_id"),
+  email: text("email").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+
+  role: text("role", { enum: ["user", "admin", "moderator"] })
+    .notNull()
+    .default("user"),
+  isActive: integer("is_active", { mode: "boolean" }).notNull().default(true),
+  isEmailVerified: integer("is_email_verified", { mode: "boolean" })
+    .notNull()
+    .default(false),
+
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(uniexpoch())`),
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(uniexpoch())`),
 });
